7991e94ad8a3f36a07be834ea99bbbd6472b6040
“[jacob-majesty]” <[jacobamony@gmail.com]>
2024-11-27 23:36:05 -0300

    Applications properties config

--- pet-care/src/main/java/com/majesty/pet_care/model/Admin.java
+++ pet-care/src/main/java/com/majesty/pet_care/model/Admin.java
@@ -2,6 +2,8 @@ package com.majesty.pet_care.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +14,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@PrimaryKeyJoinColumn(name = "admin_id")
 public class Admin extends User {
 
+    //@GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private long id;
 
 }
--- pet-care/src/main/java/com/majesty/pet_care/model/User.java
+++ pet-care/src/main/java/com/majesty/pet_care/model/User.java
@@ -1,7 +1,9 @@
 package com.majesty.pet_care.model;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Inheritance;
@@ -17,8 +19,9 @@ import lombok.Setter;
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED) 
 public class User {
-    
+
     @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private long id;
     private String firstName;
     private String lastName;
