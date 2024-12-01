1234549e639deef055cea3e8f8a09630a15e9a93
“[jacob-majesty]” <[jacobamony@gmail.com]>
2024-12-01 15:07:06 -0300

    Refactored UserController and created utils package

--- pet-care/src/main/java/com/majesty/pet_care/controller/UserController.java
+++ pet-care/src/main/java/com/majesty/pet_care/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.majesty.pet_care.controller;
-
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.majesty.pet_care.dto.EntityConverter;
 import com.majesty.pet_care.dto.UserDto;
 import com.majesty.pet_care.exception.UserAlreadyExistsException;
@@ -11,15 +12,13 @@ import com.majesty.pet_care.model.User;
 import com.majesty.pet_care.request.RegistrationRequest;
 import com.majesty.pet_care.response.ApiResponse;
 import com.majesty.pet_care.service.user.UserService;
+import com.majesty.pet_care.utils.FeedbackMessage;
+import com.majesty.pet_care.utils.UrlMapping;
 
 import lombok.RequiredArgsConstructor;
 
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-
-
-@RequestMapping("/users")
+@RequestMapping(UrlMapping.USERS)
 @RestController
 @RequiredArgsConstructor
 public class UserController {
@@ -32,12 +31,15 @@ public class UserController {
         try {
             User theUser = userService.add(request);
             UserDto registeredUser = entityConverter.mapEntityToDtoD(theUser, UserDto.class);
-            return ResponseEntity.ok(new ApiResponse("User registered successfully",registeredUser));
+            return ResponseEntity.ok(new ApiResponse(FeedbackMessage.SUCCESS,registeredUser));
 
         } catch (UserAlreadyExistsException e) {
-            return ResponseEntity.ok(new ApiResponse(e.getMessage(), null));
+            return ResponseEntity.status(Response.SC_CONFLICT).body(new ApiResponse(e.getMessage(), null));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(Response.SC_INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
 
         }
-    }
 
+    }
 }
---
+++ pet-care/src/main/java/com/majesty/pet_care/utils/UrlMapping.java
@@ -0,0 +1,8 @@
+package com.majesty.pet_care.utils;
+
+public class UrlMapping {
+    
+    public static final String API = "/api";
+    public static final String USERS = API+"/users";
+
+}
