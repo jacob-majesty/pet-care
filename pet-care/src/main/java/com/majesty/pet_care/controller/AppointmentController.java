61e14797c5790f12a641a14f5a47fe31eec4e5b3
“[jacob-majesty]” <[jacobamony@gmail.com]>
2024-12-03 07:06:55 -0300

    Working on Appointment controller

---
+++ pet-care/src/main/java/com/majesty/pet_care/controller/AppointmentController.java
@@ -0,0 +1,128 @@
+package com.majesty.pet_care.controller;
+
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.majesty.pet_care.exception.RessourceNotFoundException;
+import com.majesty.pet_care.model.Appointment;
+import com.majesty.pet_care.response.ApiResponse;
+import com.majesty.pet_care.service.appointment.AppointmentService;
+import com.majesty.pet_care.utils.FeedbackMessage;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/appointments")
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    @GetMapping("/all") 
+    public ResponseEntity<ApiResponse> getAllAppointments() {
+        try {
+            List<Appointment> appointments = appointmentService.getAllAppointments();
+            return ResponseEntity.status(Response.SC_FOUND).body(new ApiResponse(FeedbackMessage.FOUND, appointments));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(Response.SC_INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }            
+    }
+
+    @PostMapping("/book-appointment")
+    public ResponseEntity<ApiResponse> bookAppointment(
+            @RequestBody Appointment appointment,
+            @RequestParam Long senderId,
+            @RequestParam Long recipientId) {
+
+                try {
+                    Appointment theAppointment = appointmentService.createAppointment(appointment, senderId, recipientId);
+                    return ResponseEntity.status(Response.SC_OK).body(new ApiResponse(FeedbackMessage.SUCCESS, theAppointment));
+
+
+                } catch (RessourceNotFoundException e) {
+                    return ResponseEntity.status(Response.SC_NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+                    
+                } catch (Exception e) {
+                    return ResponseEntity.status(Response.SC_INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+                    
+                }            
+    }
+
+
+    @GetMapping("/appointment/{id}")
+    public ResponseEntity<ApiResponse> getAppointmentById(@PathVariable Long id) {
+        try {
+            Appointment appointment = appointmentService.getAppointmentById(id);
+            return ResponseEntity.status(Response.SC_FOUND).body(new ApiResponse(FeedbackMessage.FOUND, appointment));
+            
+        } catch (RessourceNotFoundException e) {
+            return ResponseEntity.status(Response.SC_NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+            
+        } catch (Exception e) {
+            return ResponseEntity.status(Response.SC_INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+            
+        }    
+    }
+
+    @GetMapping("/appointment/{appointmentNo}")
+    public ResponseEntity<ApiResponse> getAppointmentByNo(@PathVariable String appointmentNo) {
+        try {
+            Appointment appointment = appointmentService.getAppointmentByNo(appointmentNo);
+            return ResponseEntity.status(Response.SC_FOUND).body(new ApiResponse(FeedbackMessage.FOUND, appointment));
+            
+        } catch (RessourceNotFoundException e) {
+            return ResponseEntity.status(Response.SC_NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+            
+        } catch (Exception e) {
+            return ResponseEntity.status(Response.SC_INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+            
+        }    
+    }
+
+
+    @DeleteMapping("/appointment/{id}")
+    public ResponseEntity<ApiResponse> deleteAppointmentById(@PathVariable Long id) {
+        try {
+            appointmentService.deleteAppointment(id);
+            return ResponseEntity.status(Response.SC_FOUND).body(new ApiResponse(FeedbackMessage.DELETE_SUCCESS, null));
+            
+        } catch (RessourceNotFoundException e) {
+            return ResponseEntity.status(Response.SC_NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+            
+        } catch (Exception e) {
+            return ResponseEntity.status(Response.SC_INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+            
+        }
+    }
+
+    @PutMapping("/appointment/{id}")
+    public ResponseEntity<ApiResponse> updateAppointment(@PathVariable Long id, @RequestBody Appointment appointment) {
+        try {
+            Appointment theAppointment = appointmentService.updateAppointment(id, appointment);
+            return ResponseEntity.status(Response.SC_FOUND).body(new ApiResponse(FeedbackMessage.UPDATE_SUCCESS, theAppointment));
+            
+        } catch (RessourceNotFoundException e) {
+            return ResponseEntity.status(Response.SC_NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+            
+        } catch (Exception e) {
+            return ResponseEntity.status(Response.SC_INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+            
+        }
+    }
+
+}
--- pet-care/src/main/java/com/majesty/pet_care/utils/FeedbackMessage.java
+++ pet-care/src/main/java/com/majesty/pet_care/utils/FeedbackMessage.java
@@ -2,9 +2,9 @@ package com.majesty.pet_care.utils;
 
 public class FeedbackMessage {
 
-    public static final String SUCCESS = "User registered successfully";
-    public static final String UPDATE_SUCCESS = "User updated successfully";
-    public static final String FOUND = null;
-    public static final String DELETE_SUCCESS = "Deleted successfully";
+    public static final String SUCCESS = "Resource registered successfully";
+    public static final String UPDATE_SUCCESS = "Resource updated successfully";
+    public static final String FOUND = "Resource Found";
+    public static final String DELETE_SUCCESS = "Resource Deleted successfully";
 
 }
