38ea6b1d20c172acd1fe5ff43b37765d23a4551d
“[jacob-majesty]” <[jacobamony@gmail.com]>
2024-12-25 04:22:24 -0300

    Implementing Pet Dto

--- pet-care/src/main/java/com/majesty/pet_care/dto/PetDto.java
+++ pet-care/src/main/java/com/majesty/pet_care/dto/PetDto.java
@@ -1,5 +1,15 @@
 package com.majesty.pet_care.dto;
 
+import lombok.Data;
+
+@Data
 public class PetDto {
+
+    private long id;
+    private String name;
+    private String type;
+    private String color;
+    private String breed;
+    private int age;
 
 }
