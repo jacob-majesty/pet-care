59246b7494ee444dff99b61b99848e58952c72e6
“[jacob-majesty]” <[jacobamony@gmail.com]>
2024-12-28 23:48:07 -0300

    Second commit | Home page

---
+++ frontend/pet-care/universal-pet-care/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/home/Home";
+import BackgroundImageSlider from "./components/common/BackgroundImageSlider";
+import {
+  Route,
+  Router,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import RootLayout from "./components/layout/RootLayout";
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout />}>        
+        <Route index element={<Home />} />
+      </Route>
+    )
+  );
+  return (
+    <main className=''>
+      <RouterProvider router={router} />
+    </main>
+  );
+}
+
+export default App;
