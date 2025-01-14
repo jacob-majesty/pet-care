971ec72318a8d3de0bb014b2a73640c6b3cc8aeb
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-01-14 11:14:10 -0300

    Fix: Update Urlmapping

--- pet-care/src/main/java/com/majesty/pet_care/utils/UrlMapping.java
+++ pet-care/src/main/java/com/majesty/pet_care/utils/UrlMapping.java
@@ -1,9 +1,9 @@
 package com.majesty.pet_care.utils;
 
 public class UrlMapping {
-    
+
     public static final String API = "/api/v1";
-    public static final String USERS = API+"/users";
+    public static final String USERS = API + "/users";
     public static final String REGISTER_USER = "/register";
     public static final String UPDATE_USER = "/update/{userId}";
     public static final String FOUND = "Found";
@@ -11,8 +11,8 @@ public class UrlMapping {
     public static final String DELETE_USER_BY_ID = "/delete/{userId}";
     public static final String GET_ALL_USERS = "/all-users";
 
-    /* Appointment API */  
-    public static final String APPOINTMENTS = API+"/appointments";
+    /* Appointment API */
+    public static final String APPOINTMENTS = API + "/appointments";
     public static final String ALL_APPOINTMENTS = "/all";
     public static final String GET_APPOINTMENT_BY_NO = "/appointment/{appointmentNo}";
     public static final String GET_APPOINTMENT_BY_ID = "/appointment/{id}/appointment";
@@ -20,32 +20,31 @@ public class UrlMapping {
     public static final String UPDATE_APPOINTMENT = "/appointment/{id}/update";
     public static final String DELETE_APPOINTMENT_BY_ID = "/appointment/{id}/delete";
 
-    /* Pet API */  
-    public static final String PETS = API+"/pets";
+    /* Pet API */
+    public static final String PETS = API + "/pets";
     public static final String UPDATE_PET = "/pet/{petId}/update";
-    public static final String SAVE_PETS_FOR_APPOINTMENT = "/save-pets/"; 
+    public static final String SAVE_PETS_FOR_APPOINTMENT = "/save-pets/";
     public static final String GET_PET_BY_ID = "/pet/{petId}/pet";
     public static final String DELETE_PET_BY_ID = "/pet/{petId}/delete";
 
     /* Photo API */
-    public static final String PHOTOS = API+"/photos" ;
-    public static final String UPLOAD_PHOTO = "/photo/upload" ;
-    public static final String UPDATE_PHOTO = "/photo/{photoId}/update"; ;
-    public static final String DELETE_PHOTO = "/photo/{photoId}/user/{userId}/delete" ;
-    public static final String GET_PHOTO_BY_ID = "/photo/{photoId}/photo" ;
+    public static final String PHOTOS = API + "/photos";
+    public static final String UPLOAD_PHOTO = "/photo/upload";
+    public static final String UPDATE_PHOTO = "/photo/{photoId}/update";;
+    public static final String DELETE_PHOTO = "/photo/{photoId}/user/{userId}/delete";
+    public static final String GET_PHOTO_BY_ID = "/photo/{photoId}/photo";
 
     /* Review API */
-    public static final String REVIEWS = API+"/reviews" ;
-    public static final String SUBMIT_REVIEW = "/submit-review" ;
-    public static final String GET_USER_REVIEWS = "/user/{userId}/reviews" ;
-    public static final String UPDATE_REVIEW = "/review/{reviewId}/update" ;
-    public static final String DELETE_REVIEW = "/review/{reviewId}/delete" ;
-    public static final String GET_AVERAGE_RATING ="/vet/{vetId}/get-average-rating";
+    public static final String REVIEWS = API + "/reviews";
+    public static final String SUBMIT_REVIEW = "/submit-review";
+    public static final String GET_USER_REVIEWS = "/user/{userId}/reviews";
+    public static final String UPDATE_REVIEW = "/review/{reviewId}/update";
+    public static final String DELETE_REVIEW = "/review/{reviewId}/delete";
+    public static final String GET_AVERAGE_RATING = "/vet/{vetId}/get-average-rating";
 
     /* Veterinarian API */
-    public static final String VETERINARIANS = API+"/veterinarians";
+    public static final String VETERINARIANS = API + "/veterinarians";
     public static final String GET_ALL_VETERINARIANS = "/get-all-veterinarians";
     public static final String SEARCH_VETERINARIAN_FOR_APPOINTMENT = "/search-veterinarian";
-    ;
-   
+
 }
