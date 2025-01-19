606867b7312fd91c8338c0f64bc18f8728375d60
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-01-19 16:47:53 -0300

    Feat: Add query in review repo

--- pet-care/src/main/java/com/majesty/pet_care/repository/ReviewRepository.java
+++ pet-care/src/main/java/com/majesty/pet_care/repository/ReviewRepository.java
@@ -11,11 +11,13 @@ import com.majesty.pet_care.model.Review;
 import java.util.List;
 import java.util.Optional;
 
-
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    @Query("SELECT r FROM Review r WHERE r.patient.id =:userId OR r.veterinarian.id =:userId")    
+    @Query("SELECT r FROM Review r WHERE r.patient.id =:userId OR r.veterinarian.id =:userId")
     Page<Review> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("SELECT r FROM Review r WHERE r.patient.id =:userId OR r.veterinarian.id =:userId ")
+    List<Review> findAllByUserId(@Param("userId") Long userId);
 
     List<Review> findByVeterinarianId(Long veterinarianId);
 
