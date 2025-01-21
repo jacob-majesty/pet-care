243d17a9c4a8f3d50d612e6ed7b0a3e06b80cc68
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-01-21 10:37:35 -0300

    Feat: Add Jwt Utils

--- pet-care/src/main/java/com/majesty/pet_care/security/jwt/JwtUtils.java
+++ pet-care/src/main/java/com/majesty/pet_care/security/jwt/JwtUtils.java
@@ -1,5 +1,68 @@
 package com.majesty.pet_care.security.jwt;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import com.majesty.pet_care.security.user.UPCUserDetails;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
 public class JwtUtils {
+
+    @Value("${auth.token.jwtSecret}")
+    private String jwtSecret;
+    @Value("${auth.token.expirationInMils}")
+    private int jwtExpirationMs;
+
+    public String generateTokenForUser(Authentication authentication) {
+        UPCUserDetails userPrincipal = (UPCUserDetails) authentication.getPrincipal();
+
+        List<String> roles = userPrincipal.getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority)
+                .toList();
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .claim("id", userPrincipal.getId())
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUserNameFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(token);
+            return true;
+        } catch (MalformedJwtException | IllegalArgumentException | UnsupportedJwtException | ExpiredJwtException e) {
+            throw new JwtException(e.getMessage());
+        }
+
+    }
 
 }
