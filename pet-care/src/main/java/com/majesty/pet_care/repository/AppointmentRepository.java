5e4ba86fe3b5ae1fc9e1c13d26fb5d8a328398eb
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-02-02 14:59:10 -0300

    Fix in appointment repo

--- pet-care/src/main/java/com/majesty/pet_care/repository/AppointmentRepository.java
+++ pet-care/src/main/java/com/majesty/pet_care/repository/AppointmentRepository.java
@@ -12,15 +12,14 @@ import com.majesty.pet_care.model.User;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
-    Appointment findByAppointmentNo(String appointmentNo);
+        Appointment findByAppointmentNo(String appointmentNo);
 
-    boolean existsByPatientIdAndVeterinarianIdAndStatus(Long reviewerId, Long veterinarianId,
-            AppointmentStatus appointmentStatus);
-    
-    @Query("SELECT a FROM Appointment a WHERE a.patient.id =:userId OR a.veterinarian.id =:userId")  
+        boolean existsByPatientIdAndVeterinarianIdAndStatus(Long reviewerId, Long veterinarianId,
+                        AppointmentStatus appointmentStatus);
 
-    List<Appointment> findAllByUserId(Long userId);
+        @Query("SELECT a FROM Appointment a WHERE a.patient.id =:userId OR a.veterinarian.id =:userId")
+        List<Appointment> findAllByUserId(Long userId);
 
-     List<Appointment> findByVeterinarianAndAppointmentDate(User veterinarian, LocalDate requestedDate);
+        List<Appointment> findByVeterinarianAndAppointmentDate(User veterinarian, LocalDate requestedDate);
 
 }
