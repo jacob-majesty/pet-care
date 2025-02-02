5273a5ba4b6d8eac8ea0bdf9966d3ac27465abb7
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-02-02 05:21:33 -0300

    Feat: Add features to factory

--- pet-care/src/main/java/com/majesty/pet_care/factory/AdminFactory.java
+++ pet-care/src/main/java/com/majesty/pet_care/factory/AdminFactory.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.majesty.pet_care.model.Admin;
 import com.majesty.pet_care.repository.AdminRepository;
 import com.majesty.pet_care.request.RegistrationRequest;
+import com.majesty.pet_care.service.role.IRoleService;
 import com.majesty.pet_care.service.user.UserAttributeMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,14 @@ public class AdminFactory {
 
     private final AdminRepository adminRepository;
     private final UserAttributeMapper userAttributeMapper;
+    private final IRoleService roleService;
 
     public Admin createAdmin(RegistrationRequest request) {
         Admin admin = new Admin();
+        admin.setRoles(roleService.setUserRole("ADMIN"));
         userAttributeMapper.setCommonAttributes(request, admin);
         return adminRepository.save(admin);
-        
+
     }
 
 }
--- pet-care/src/main/java/com/majesty/pet_care/factory/PatientFactory.java
+++ pet-care/src/main/java/com/majesty/pet_care/factory/PatientFactory.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.majesty.pet_care.model.Patient;
 import com.majesty.pet_care.repository.PatientRepository;
 import com.majesty.pet_care.request.RegistrationRequest;
+import com.majesty.pet_care.service.role.IRoleService;
 import com.majesty.pet_care.service.user.UserAttributeMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,14 @@ public class PatientFactory {
 
     private final PatientRepository patientRepository;
     private final UserAttributeMapper userAttributeMapper;
+    private final IRoleService roleService;
 
     public Patient createPatient(RegistrationRequest request) {
         Patient patient = new Patient();
+        patient.setRoles(roleService.setUserRole("PATIENT"));
         userAttributeMapper.setCommonAttributes(request, patient);
         return patientRepository.save(patient);
-        
+
     }
 
 }
