30552cd35f33964a6eb7eee567aa98382091fa3a
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-02-02 02:45:59 -0300

    Feat: Add features to review

--- frontend/pet-care/universal-pet-care/src/components/review/ReviewService.js
+++ frontend/pet-care/universal-pet-care/src/components/review/ReviewService.js
@@ -2,9 +2,14 @@ import { api } from "../utils/api";
 
 export async function addReview(vetId, reviewerId, reviewData) {
   try {
+    const token = localStorage.getItem("authToken")
     const response = await api.post(
       `reviews/submit-review?vetId=${vetId}&reviewerId=${reviewerId}`,
-      reviewData
+      reviewData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
     return response.data;
   } catch (error) {
