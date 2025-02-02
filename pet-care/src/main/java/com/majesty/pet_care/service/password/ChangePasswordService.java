904322f42b3824af0047a873f0177dc1d3fde6b2
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-02-02 05:21:52 -0300

    Feat: Add features to password

--- pet-care/src/main/java/com/majesty/pet_care/service/password/ChangePasswordService.java
+++ pet-care/src/main/java/com/majesty/pet_care/service/password/ChangePasswordService.java
@@ -1,6 +1,8 @@
 package com.majesty.pet_care.service.password;
 
 import java.util.Objects;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.majesty.pet_care.exception.ResourceNotFoundException;
 import com.majesty.pet_care.model.User;
@@ -12,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChangePasswordService implements IChangePasswordService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void changePassword(Long userId, ChangePasswordRequest request) {
@@ -20,15 +23,14 @@ public class ChangePasswordService implements IChangePasswordService {
                 || Objects.equals(request.getNewPassword(), "")) {
             throw new IllegalArgumentException("All fields are required");
         }
-        if (!Objects.equals(request.getCurrentPassword(), user.getPassword())) {
+        if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
             throw new IllegalArgumentException("Current password does not match");
         }
-        if (!Objects.equals(request.getNewPassword(), request.getConfirmNewPassword())) {
+        if (!request.getNewPassword().equals(request.getConfirmNewPassword())) {
             throw new IllegalArgumentException("Password confirmation mis-match ");
         }
-        user.setPassword(request.getNewPassword());
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
         userRepository.save(user);
 
     }
-
 }
